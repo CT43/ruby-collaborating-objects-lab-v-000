100dd2d9bdf98a75c95267495183b9f6d2378e36
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-03 16:02:56 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,8 +10,8 @@ class Artist
   end
 
   def add_song(song)
-    @songs << song unless @songs.include?(song) 
-    song.artist = self
+    @songs << song unless @songs.include?(song)
+    song.artist = self unless song.artist == self
   end
 
   def save
