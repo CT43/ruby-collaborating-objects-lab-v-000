248b33c1b626750785f77d1781b821dc186d211b
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-03 15:56:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,7 @@ class Song
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    
-
+    artist.add_song(self) unless artist.songs.include?(self)
   end
 
   def self.new_by_filename(file_name)
